854565a1119fc8a26541d60320bd28d7e1c35a54
Jerocampos <ocamposjero@gmail.com>
2017-02-12 19:18:06 -0500

    Updated a lot!

---
+++ bootstrap-3.3.7-dist/js/thumbnail.js
@@ -0,0 +1,30 @@
+/**
+ * Created by JeronimoJ on 2/12/2017.
+ */
+function goTo(number){
+    $('ul.pager li:eq('+number+') a').tab('show');
+    upgradePreNext(number);
+}
+function upgradePreNext(number){
+    if (number>1){
+        $('ul.pager li:eq(0)').attr("onclick","goTo("+(number-1)+")");
+        $('ul.pager li:eq(0)').attr("class", "previous");
+    } else {
+        $('ul.pager li:eq(0)').attr("class", "disabled");
+    }
+    if (number<5){
+        $('ul.pager li:eq(6)').attr("onclick","goTo("+(number+1)+")");
+        $('ul.pager li:eq(6)').attr("class", "next");
+    } else {
+        $('ul.pager li:eq(6)').attr("class", "disabled");
+    }
+}
+$(document).ready(function(){
+    $('li a').on('click',function(e){
+        goTo((e.target.innerHTML)-0);
+    });
+
+    $('#myCarousel').carousel({
+        interval: 10000
+    })
+});
