ad13b414b1f4dd108501c33a4d1be27816c1cad3
chimercial <namann@Nathaniels-MacBook.local>
2017-04-03 15:32:09 -0400

    cleaned up

--- index.php
+++ index.php
@@ -136,47 +136,6 @@ if (isset($_POST['logout'])){
                 </div>
                 </div>
 
-        <div class="panel panel-default">
-                    <div class="panel-heading">
-                    <h3 class="panel-title">
-                    <span class="glyphicon glyphicon-eye-open"></span>
-                    Recently View
-                    </h3>
-                    </div>
-                    <div class="panel-body">
-
-
-                            <div id="myCarousel" class="carousel slide">
-
-
-                                    <!-- Carousel items -->
-                                    <div class="carousel-inner">
-
-                                        <div class="item active">
-                                            <div class="row-fluid">
-                                                <div class="span3"><a href="#x" class="thumbnail"><img src="http://placehold.it/240x150" alt="Image" style="max-width:100%;" /></a></div>
-                                            </div><!--/row-fluid-->
-                                        </div><!--/item-->
-
-                                        <div class="item">
-                                            <div class="row-fluid">
-                                                <div class="span3"><a href="#x" class="thumbnail"><img src="http://placehold.it/240x150" alt="Image" style="max-width:100%;" /></a></div>
-                                             </div><!--/row-fluid-->
-                                        </div><!--/item-->
-
-                                        <div class="item">
-                                            <div class="row-fluid">
-                                                <div class="span3"><a href="#x" class="thumbnail"><img src="http://placehold.it/240x150" alt="Image" style="max-width:100%;" /></a></div>
-                                            </div><!--/row-fluid-->
-                                        </div><!--/item-->
-                                    </div><!--/carousel-inner-->
-
-                                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
-                                    <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
-                            </div><!--/myCarousel-->
-
-                    </div>
-                </div>
 
     </div>
 
@@ -370,47 +329,6 @@ if (isset($_POST['logout'])){
 
 </div>
 
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-sm-6 footerleft ">
-                <div class="logofooter"> Logo</div>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
-                <p><i class="fa fa-map-pin"></i> 800, Florida Ave NE, Washington, DC 20002, USA</p>
-                <p><i class="fa fa-phone"></i> Phone (USA) : +1 9999 878 398</p>
-                <p><i class="fa fa-envelope"></i> E-mail : collegestuff@gmail.com</p>
-
-            </div>
-            <div class="col-md-2 col-sm-6 paddingtop-bottom">
-                <h6 class="heading7">GENERAL LINKS</h6>
-                <ul class="footer-ul">
-                    <li><a href="#"> Career</a></li>
-                    <li><a href="#"> Privacy Policy</a></li>
-                    <li><a href="#"> Terms & Conditions</a></li>
-                    <li><a href="#"> Client Gateway</a></li>
-                    <li><a href="#"> Ranking</a></li>
-                    <li><a href="#"> Case Studies</a></li>
-                    <li><a href="#"> Frequently Ask Questions</a></li>
-                </ul>
-            </div>
-            <div class="col-md-3 col-sm-6 paddingtop-bottom">
-                <h6 class="heading7">LATEST POST</h6>
-                <div class="post">
-                    <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
-                    <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
-                    <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6 paddingtop-bottom">
-                <div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-height="300" data-small-header="false" style="margin-bottom:15px;" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-                    <div class="fb-xfbml-parse-ignore">
-                        <blockquote cite="https://www.facebook.com/facebook"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
 
 <div class="copyright">
     <div class="container">
@@ -422,7 +340,6 @@ if (isset($_POST['logout'])){
                 <li><a href="#">collegestuff.com</a></li>
                 <li><a href="#">About us</a></li>
                 <li><a href="#">Contact us</a></li>
-                <li><a href="#">Site Map</a></li>
             </ul>
         </div>
     </div>
